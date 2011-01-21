0753c4f78b3b3e8b6b0142061a272f13e065e428
Michael Kerscher <git@kerscher-michael.de>
2011-01-21 01:01:35 +0000

    big redesign. work in progress

--- parser.hs
+++ parser.hs
@@ -2,61 +2,155 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec
 
-type Date = String
-type Nick = String
-type Mode = String
+data Nick = Nick String deriving Show
+data ChanMode = ChanMode String deriving Show
+data Date = Date String deriving Show
 data Time = Time String String -- "00:00" -> 00 00
-instance Show Time where show (Time hour minute) = hour ++ ":" ++ minute
+data DateTime = DateTime Date Time | DateWOTime Date
+instance Show Time where
+    show (Time hour minute) = hour ++ ":" ++ minute
+instance Show DateTime where
+    show (DateTime date time) = show date ++ "T" ++ show time
+    show (DateWOTime date) = show date
 
-data Event = LogOpen | LogClose | DayChanged Date |
-                Mode [Nick] Mode | NickAction Nick String |
+data Event = LogOpen DateTime| LogClose DateTime | DayChanged Date |
+                Mode [Nick] ChanMode | NickAction Nick String |
                 TopicChange Nick String | Kick Nick Nick String |
                 NickChange Nick Nick | Join Nick | Quit Nick String |
-                Part Nick String | Message Nick String
+                Part Nick String | Message Nick String | NotImplemented String
 
 instance Show Event where
-    show LogOpen = "log opened"
-    show LogClose = "log closed"
-    show (DayChanged date) = "day changed to " ++ date
-    show (Mode nicks mode) = "mode changed to " ++ mode ++ " for " ++ show nicks
-    show (NickAction nick action) = nick ++ " " ++ action
-    show (TopicChange nick topic) = nick ++ " changed the topic to " ++ topic
-    show (Kick kicker kicked reason) = kicker ++ " kicked " ++ kicked ++ " because " ++ reason
-    show (NickChange oldnick newnick) = oldnick ++ " changed his nick to " ++ newnick
-    show (Join nick) = nick ++ " joined"
-    show (Quit nick reason) = nick ++ " has quit because " ++ reason
-    show (Part nick reason) = nick ++ " has parted because " ++ reason
-    show (Message nick text) = nick ++ ": " ++ text
+    show (LogOpen date) = "log opened on " ++ show date
+    show (LogClose date) = "log closed on " ++ show date
+    show (DayChanged date) = "day changed to " ++ show date
+    show (Mode nicks mode) = "mode changed to " ++ show mode ++ " for " ++ show nicks
+    show (NickAction nick action) = show nick ++ " " ++ show action
+    show (TopicChange nick topic) = show nick ++ " changed the topic to " ++ show topic
+    show (Kick kicker kicked reason) = show kicker ++ " kicked " ++ show kicked ++ " because " ++ show reason
+    show (NickChange oldnick newnick) = show oldnick ++ " changed his nick to " ++ show newnick
+    show (Join nick) = show nick ++ " joined"
+    show (Quit nick reason) = show nick ++ " has quit because " ++ show reason
+    show (Part nick reason) = show nick ++ " has parted because " ++ show reason
+    show (Message nick text) = show nick ++ ": " ++ show text
+    show (NotImplemented txt) = "NOT IMPLEMENTED YET: " ++ txt
 
-data TimedEvent = IrcEvent Time Event
-
+data TimedEvent = TimedEvent Time Event
+instance Show TimedEvent where
+    show (TimedEvent time event) = show time ++ " " ++ show event
+    
 parseTime :: Parser Time
-parseTime = do
-                hour <- count 2 digit
-                char ':'
-                minute <- count 2 digit
-                return $ Time hour minute
+parseTime =
+    do
+        hour <- count 2 digit
+        char ':'
+        minute <- count 2 digit
+        try (option "00" (char ':' >> count 2 digit)) -- TODO: seconds
+        many space
+        return $ Time hour minute
 
 --special    =  %x5B-60 / %x7B-7D ; "[", "]", "\", "`", "_", "^", "{", "|", "}"
-nickSpecial = oneOf "[]\\'_^{|}"
+nickSpecial = oneOf "[]\\'_^{|}" <|> oneOf "`"
+
+parseMode :: Parser ChanMode
+parseMode = oneOf " @%+&~" >>= \mode -> return $ ChanMode [mode]
 
 --nickname   =  ( letter / special ) *8( letter / digit / special / "-" )
 parseNick :: Parser Nick
-parseNick = do
-                head <- letter <|> nickSpecial
-                tail <- many1 (alphaNum <|> nickSpecial <|> char '-')
-                return (head : tail)
-              
+parseNick =
+    do
+        head <- letter <|> nickSpecial
+        tail <- many1 (alphaNum <|> nickSpecial <|> char '-')
+        return $ Nick (head : tail)
 
-irssiParser = parseNick
+parseDateTime :: Parser DateTime
+parseDateTime =
+    do
+        dayName <- count 2 letter; space
+        monthName <- count 3 letter; space
+        day <- count 2 digit; space
+        time <- try parseTime --option (Time "00" "00") parseTime
+        year <- count 4 digit
+        -- TODO: this is just a temporary place to scan the newline!
+        newline
+        -- TODO: verifiy the date by using lib functions
+        return $ DateTime (Date (dayName ++ " " ++ monthName ++ " " ++ day ++ " " ++ year)) time
 
-readLine :: String -> String
-readLine input = case parse irssiParser "irssi log" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Yeah, found data: " ++ show val
+parseDate :: Parser DateTime
+parseDate =
+    do
+        dayName <- count 2 letter; space
+        monthName <- count 3 letter; space
+        day <- count 2 digit; space
+        year <- count 4 digit
+        -- TODO: this is just a temporary place to scan the newline!
+        newline
+        -- TODO: verifiy the date by using lib functions
+        return $ DateWOTime (Date (dayName ++ " " ++ monthName ++ " " ++ day ++ " " ++ year))
 
+parseText :: Parser String
+parseText = manyTill anyChar (try newline)
+
+parseTopic :: Parser String
+parseTopic =
+    do
+        many space
+        string "changed the topic of "
+        channel <- many (alphaNum <|> oneOf "#.")
+        string "to: "
+        text <- parseText
+        return $ text
+
+parseSpecialAction :: Parser Event
+parseSpecialAction = 
+    do
+        nick <- parseNick
+        try (parseTopic >>= \topic -> return $ TopicChange nick topic) <|>
+            (parseText >>= \dummy -> return $ NotImplemented dummy)
+
+parseMessage :: Parser Event
+parseMessage = 
+    do
+        string "<"
+        mode <- parseMode
+        nick <- parseNick
+        string "> "
+        message <- parseText
+        return $ Message nick message
+
+parseAction :: Parser Event
+parseAction =
+    do
+        string "* "
+        nick <- parseNick
+        action <- parseText
+        return $ NickAction nick action
+
+parseSpecialEvent :: Parser TimedEvent
+parseSpecialEvent =
+    do
+        string "--- "
+        (string "Day changed " >> parseDate >>= \(DateWOTime date) -> return $ TimedEvent (Time "00" "00") (DayChanged date)) <|>
+            try (string "Log opened " >> parseDateTime >>= \(DateTime date time) -> return $ TimedEvent time (LogOpen $ DateTime date time)) <|>
+            (string "Log closed " >> parseDateTime >>= \(DateTime date time) -> return $ TimedEvent time (LogClose $ DateTime date time))
+
+parseNormalEvent :: Parser TimedEvent
+parseNormalEvent =
+    do
+        time <- parseTime;
+        event <- ((string "-!- " >> parseSpecialAction) <|> parseAction <|> parseMessage)
+        return $ TimedEvent time event
+        
+parseLine :: Parser TimedEvent
+parseLine = parseSpecialEvent <|> parseNormalEvent
+
+irssiParser :: Parser [TimedEvent]
+irssiParser = many parseLine
 
 main :: IO ()
-main = do
+main =
+    do
         args <- getArgs
-        putStrLn (readLine (args !! 0))
+        result <- parseFromFile irssiParser (args !! 0)
+        case (result) of
+            Left err  -> print err
+            Right val  -> putStr $ unwords (fmap ((++ "\n") . show) (take 10 val))
